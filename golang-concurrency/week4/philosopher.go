d6ea634ea0e110a70ed0354aa9fde8cc80820264
GuangMin Cui <cuiguangmin@gmail.com>
2020-11-24 14:49:43 +0900

    golang-concurrency, fix week4

--- golang-concurrency/week4/philosopher.go
+++ golang-concurrency/week4/philosopher.go
@@ -19,14 +19,15 @@ func (p *Philo) eat(wg *sync.WaitGroup) {
 	for {
 		if p.times == 3 {
 			fmt.Printf("finishing eating %d\n", p.id)
-            wg.Done()
-            return
+			wg.Done()
+			return
 		}
 
 		p.leftCS.Lock()
 		p.rightCS.Lock()
 
 		p.times += 1
+		fmt.Printf("start eating philosopher %d the %d times\n", p.id, p.times)
 
 		p.rightCS.Unlock()
 		p.leftCS.Unlock()
@@ -45,15 +46,15 @@ func main() {
 		philos[i] = &Philo{CSticks[i], CSticks[(i+1)%5], 0, &host, i + 1}
 	}
 
-    var wg sync.WaitGroup
+	var wg sync.WaitGroup
 	for i := 0; i < 5; i++ {
-        wg.Add(1)
+		wg.Add(1)
 		go philos[i].eat(&wg)
 	}
 
-    // buffer size of 2 make sure that no more than 2 philosophers to eat concurrently.
+	// buffer size of 2 make sure that no more than 2 philosophers to eat concurrently.
 	for i := 0; i < 5; i++ {
 		host <- 1
 	}
-    wg.Wait()
+	wg.Wait()
 }
