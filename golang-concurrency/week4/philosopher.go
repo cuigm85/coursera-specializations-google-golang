f9b3dc1e5a1b7be0df311c59c0bd5798583d4f9c
GuangMin Cui <cuiguangmin@gmail.com>
2020-11-25 12:49:24 +0900

    golang-concurrency, fix week4

--- golang-concurrency/week4/philosopher.go
+++ golang-concurrency/week4/philosopher.go
@@ -3,15 +3,17 @@ package main
 import (
 	"fmt"
 	"sync"
+    "time"
 )
 
 type ChopS struct{ sync.Mutex }
 
 type Philo struct {
-	leftCS, rightCS *ChopS
-	times           int
-	host            *chan int
-	id              int
+	leftCS  *chan int
+	rightCS *chan int
+	times   int
+	host    *chan int
+	id      int
 }
 
 func (p *Philo) eat(wg *sync.WaitGroup) {
@@ -20,37 +22,83 @@ func (p *Philo) eat(wg *sync.WaitGroup) {
 			wg.Done()
 			return
 		}
-
 		<-*p.host
-		p.leftCS.Lock()
-		p.rightCS.Lock()
+
+		var handOne int
+		var handTwo int
+		// random pick up
+		select {
+		case handOne = <-*p.leftCS:
+		case handOne = <-*p.rightCS:
+		default:
+			handOne = -1
+		}
+        if handOne == -1 {
+			*p.host <- 1
+            continue
+        }
+        if handOne == p.id - 1 { // handOne is left
+		    select {
+		    case handTwo = <-*p.rightCS:
+		    default:
+			    handTwo = -1
+		    }
+            if handTwo == -1 {
+                *p.leftCS <- handOne
+			    *p.host <- 1
+                continue
+            }
+        } else {
+		    select {
+		    case handTwo = <-*p.leftCS:
+		    default:
+			    handTwo = -1
+		    }
+            if handTwo == -1 {
+                *p.rightCS <- handOne
+			    *p.host <- 1
+                continue
+            }
+        }
+
 
 		p.times += 1
 		fmt.Printf("philosopher %d start eating %d time(s)\n", p.id, p.times)
-
-		p.rightCS.Unlock()
-		p.leftCS.Unlock()
+        time.Sleep(10 * time.Millisecond)
+        if handOne == p.id - 1 { // handOne is left
+		    *p.rightCS <-handTwo
+		    *p.leftCS <- handOne
+        } else {
+		    *p.leftCS <-handTwo
+		    *p.rightCS <-handOne
+        }
 		fmt.Printf("philosopher %d finishing eating %d time(s)\n", p.id, p.times)
 		*p.host <- 1
 	}
 }
 
 func main() {
-	CSticks := make([]*ChopS, 5)
-	for i := 0; i < 5; i++ {
-		CSticks[i] = new(ChopS)
+	const philosNo = 5
+	host := make(chan int, 2)
+	var CSticks [philosNo]chan int
+	for i := range CSticks {
+		CSticks[i] = make(chan int, 1)
 	}
 
-	host := make(chan int, 2)
-	philos := make([]*Philo, 5)
-	for i := 0; i < 5; i++ {
-		philos[i] = &Philo{CSticks[i], CSticks[(i+1)%5], 0, &host, i + 1}
+	philos := make([]*Philo, philosNo)
+	for i := 0; i < philosNo; i++ {
+		philos[i] = &Philo{&CSticks[i], &CSticks[(i+1)%philosNo], 0, &host, i + 1}
 	}
 
 	var wg sync.WaitGroup
-	for i := 0; i < 5; i++ {
-		wg.Add(1)
+	wg.Add(philosNo)
+	for i := 0; i < philosNo; i++ {
 		go philos[i].eat(&wg)
+	}
+
+	// host put CSticks on the table
+	for i := range CSticks {
+		CSticks[i] <- i
 	}
 
 	// buffer size of 2 make sure that no more than 2 philosophers to eat concurrently.
